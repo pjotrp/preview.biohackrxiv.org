765947de4fbc052b9bbda1b0d7df40de02656465
Tazro Inutano Ohta <inutano@gmail.com>
2021-07-06 11:09:34 +0900

    app_root somehow doesnot work with production env

--- app.rb
+++ app.rb
@@ -33,10 +33,6 @@ end
 
 class BHXIV < Sinatra::Base
   helpers do
-    def app_root
-      "#{env["rack.url_scheme"]}://#{env["HTTP_HOST"]}#{env["SCRIPT_NAME"]}"
-    end
-    
     def create_workdir(id)
       workdir = "/tmp/#{id}"
       FileUtils.mkdir(workdir)
